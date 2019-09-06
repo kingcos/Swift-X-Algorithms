35a7e4880ca5bcae0020be7a9a7b95ce18e58053
kingcos <2821836721v@gmail.com>
2019-09-07 00:26:32 +0800

    Add LC-747

---
+++ LeetCode/747-Largest-Number-At-Least-Twice-of-Others/747-Largest-Number-At-Least-Twice-of-Others.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class Solution {
+    func dominantIndex(_ nums: [Int]) -> Int {
+        guard !nums.isEmpty else { return -1 }
+        
+        var maxIndex = 0
+        
+        for t in nums.enumerated() {
+            maxIndex = t.element >= nums[maxIndex] ? t.offset : maxIndex
+        }
+        
+        for t in nums.enumerated() {
+            guard t.offset != maxIndex else { continue }
+            
+            if t.element * 2 > nums[maxIndex] {
+                return -1
+            }
+        }
+        
+        return  maxIndex
+    }
+}
+
+print(Solution().dominantIndex([1,0]))
---
+++ LeetCode/747-Largest-Number-At-Least-Twice-of-Others/README.md
@@ -0,0 +1,26 @@
+# Swift-X-Algorithms
+
+> Algorithms implemented in Swift/Go/Python.
+
+## 747 Largest Number At Least Twice of Others
+
+### Solutions
+
+#### Swift
+
+```Swift
+```
+
+#### Go
+
+```go
+```
+
+#### Python
+
+```python
+```
+
+## License
+
+- MIT
