988436c291d2849db9bb060bb592d2e063ce580a
kingcos <2821836721v@gmail.com>
2021-03-14 13:19:43 +0800

    Updated

--- LeetCode/021-Merge-Two-Sorted-Lists/021-Merge-Two-Sorted-Lists.playground/Contents.swift
+++ LeetCode/021-Merge-Two-Sorted-Lists/021-Merge-Two-Sorted-Lists.playground/Contents.swift
@@ -1,12 +1,22 @@
 import UIKit
 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+
 public class ListNode {
     public var val: Int
     public var next: ListNode?
-    public init(_ val: Int) {
-        self.val = val
-        self.next = nil
-    }
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
 }
 
 class Solution_1 {
